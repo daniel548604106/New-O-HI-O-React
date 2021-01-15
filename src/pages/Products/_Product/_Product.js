c0c95820583cdcd2f227a75d590a961453407bd8
hiskio-daniel <daniel.yeh@hiskio.com>
2021-01-15 13:02:42 +0800

    product layout complete

--- src/pages/Products/_Product/_Product.js
+++ src/pages/Products/_Product/_Product.js
@@ -1,18 +1,39 @@
 import { makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 import StarIcon from '@material-ui/icons/Star';
-import { Divider } from '@material-ui/core';
+import { Divider, IconButton, Button } from '@material-ui/core';
 import { product } from '../../../lib/fakeData';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: '15px',
     maxWidth: '1000px',
+    margin: '0 auto',
+  },
+  main: {
     display: 'block',
+
     [theme.breakpoints.up('800')]: {
       display: 'flex',
       justifyContent: 'space-between',
     },
-    margin: '0 auto',
+  },
+  productName: {
+    [theme.breakpoints.up('800')]: {
+      fontSize: '22px',
+    },
+  },
+  mainInfo: {
+    minWidth: '300px',
+    [theme.breakpoints.up('800')]: {
+      marginLeft: '30px',
+    },
+  },
+  disabled: {
+    backgroundColor: 'gray',
+    color: 'black',
   },
   imageTab: {
     width: '40px',
@@ -35,6 +56,11 @@ const useStyles = makeStyles((theme) => ({
   activeColor: {
     border: 'black',
   },
+  removeBtn: {
+    padding: '5px',
+    backgroundColor: 'black',
+    color: 'white',
+  },
   activeTab: {
     color: 'black',
     fontWeight: 500,
@@ -47,125 +73,189 @@ const Product = () => {
   const [activeSize, setActiveSize] = useState(0);
   const [activeColor, setActiveColor] = useState(0);
   const [activeTab, setActiveTab] = useState(0);
+  const [numOfPurchase, setNumOfPurchase] = useState(1);
+  const decrement = () => {
+    if (numOfPurchase === 1) return;
+    setNumOfPurchase((prevNum) => (prevNum > 1 ? (prevNum -= 1) : null));
+  };
   return (
     <div className={classes.root}>
-      <div
-        className="carousel"
-        style={{
-          width: '100%',
-          position: 'relative',
-          margin: '0 auto',
-          maxWidth: '700px',
-          paddingTop: '60%',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          borderRadius: '20px',
-          backgroundImage: `url(${product.images[activeImage]})`,
-        }}
-      >
+      <div className={classes.main}>
         <div
+          className="carousel"
           style={{
-            display: 'flex',
-            left: '50%',
-            transform: 'translate(-50%,0)',
-            position: 'absolute',
-            bottom: '5%',
+            width: '100%',
+            position: 'relative',
+            margin: '0 auto',
+            maxWidth: '600px',
+            paddingTop: '60%',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            borderRadius: '20px',
+            backgroundImage: `url(${product.images[activeImage]})`,
           }}
         >
-          {product.images.map((image, idx) => (
-            <img
-              src={image}
-              className={classes.imageTab}
-              key={image}
-              onClick={() => setActiveImage(idx)}
-              alt=""
-            />
-          ))}
-        </div>
-      </div>
-      <div style={{ flex: 1 }}>
-        <h3 style={{ width: '90%', marginTop: '10px' }}>{product.name}</h3>
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <div style={{ display: 'flex', alignItems: 'center', margin: '5px 0' }}>
-            <StarIcon style={{ width: '20px', height: '20px', color: '#f0d90a' }} />
-            <p style={{ fontSize: '14px' }}>
-              {product.ratings} <span style={{ color: 'gray' }}>({product.numOfRatings})</span>
-            </p>
-          </div>
-          <div>
-            <span style={{ fontWeight: 500, fontSize: '16px', marginRight: '10px' }}>
-              $ {product.price}
-            </span>
-            <span style={{ textDecoration: 'line-through', fontSize: '12px', color: 'gray' }}>
-              $ {product.originalPrice}
-            </span>
-          </div>
-        </div>
-        <Divider style={{ margin: '10px 0 ' }} />
-        <div>
-          <h5 style={{ margin: '10px 0' }}>Available Sizes</h5>
-          <div style={{ display: 'flex' }}>
-            {product.availableSizes.map((sizes, idx) => (
-              <p
-                onClick={() => setActiveSize(idx)}
-                className={activeSize === idx ? classes.activeSize : null}
-                style={{
-                  width: '30px',
-                  height: '30px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderRadius: '99999px',
-                  border: '1px solid gray',
-                  marginRight: '10px',
-                }}
-                key={sizes.size}
-              >
-                {sizes.size}
-              </p>
+          <div
+            style={{
+              display: 'flex',
+              left: '50%',
+              transform: 'translate(-50%,0)',
+              position: 'absolute',
+              bottom: '5%',
+            }}
+          >
+            {product.images.map((image, idx) => (
+              <img
+                src={image}
+                className={classes.imageTab}
+                key={image}
+                onClick={() => setActiveImage(idx)}
+                alt=""
+              />
             ))}
           </div>
-          <h5 style={{ margin: '10px 0' }}>Colors</h5>
-          <div style={{ display: 'flex' }}>
-            {product.availableSizes.map((sizes) => {
-              sizes.stocks.map((stock) => (
-                <div
-                  key={stock.color}
-                  style={{
-                    backgroundColor: `${stock.hex}`,
-                    width: '20px',
-                    height: '20px',
-                    border: '1px solid black',
-                  }}
-                >
-                  {stock.color}
-                </div>
-              ));
-            })}
-          </div>
-          <div style={{ display: 'flex' }}>
-            <h3
-              onClick={() => setActiveTab(0)}
-              className={activeTab === 0 ? classes.activeTab : null}
-              style={{ fontSize: ' 16px', margin: '10px 20px 0 0px', color: 'gray' }}
-            >
-              Description
-            </h3>
-            <h3
-              onClick={() => setActiveTab(1)}
-              className={activeTab === 1 && classes.activeTab}
-              style={{ fontSize: ' 16px', margin: '10px 0', color: 'gray' }}
-            >
-              Reviews
-            </h3>
-          </div>
-          {activeTab === 0 ? (
-            <p style={{ fontSize: '14px' }}>{product.description}</p>
-          ) : (
-            <p style={{ fontSize: '14px' }}>{product.reviews}</p>
-          )}
         </div>
+        <div className={classes.mainInfo}>
+          <h3 style={{ marginTop: '10px', marginBottom: '10px' }} className={classes.productName}>
+            {product.name}
+          </h3>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center', margin: '5px 0' }}>
+              <StarIcon style={{ width: '20px', height: '20px', color: '#f0d90a' }} />
+              <p style={{ fontSize: '14px' }}>
+                {product.ratings} <span style={{ color: 'gray' }}>({product.numOfRatings})</span>
+              </p>
+            </div>
+            <div>
+              <span style={{ fontWeight: 500, fontSize: '16px', marginRight: '10px' }}>
+                $ {product.price}
+              </span>
+              <span style={{ textDecoration: 'line-through', fontSize: '12px', color: 'gray' }}>
+                $ {product.originalPrice}
+              </span>
+            </div>
+          </div>
+          <Divider style={{ margin: '10px 0 ' }} />
+          <div>
+            <h5 style={{ margin: '10px 0' }}>Available Sizes</h5>
+            <div style={{ display: 'flex' }}>
+              {product.availableSizes.map((sizes, idx) => (
+                <p
+                  onClick={() => setActiveSize(idx)}
+                  className={activeSize === idx ? classes.activeSize : null}
+                  style={{
+                    width: '30px',
+                    height: '30px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: '99999px',
+                    border: '1px solid gray',
+                    marginRight: '10px',
+                  }}
+                  key={sizes.size}
+                >
+                  {sizes.size}
+                </p>
+              ))}
+            </div>
+            <h5 style={{ margin: '10px 0' }}>Colors</h5>
+            <div style={{ display: 'flex' }}>
+              {product.availableSizes.map((sizes) => {
+                sizes.stocks.map((stock) => (
+                  <div
+                    key={stock.color}
+                    style={{
+                      backgroundColor: `${stock.hex}`,
+                      width: '20px',
+                      height: '20px',
+                      border: '1px solid black',
+                    }}
+                  >
+                    {stock.color}
+                  </div>
+                ));
+              })}
+            </div>
+
+            <div
+              className="button"
+              style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+            >
+              <h4>Quantity</h4>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <RemoveIcon
+                  onClick={decrement}
+                  className={(numOfPurchase === 1 && classes.disabled, classes.removeBtn)}
+                  style={{ cursor: 'pointer' }}
+                ></RemoveIcon>
+                <span style={{ margin: '10px 20px ' }}>{numOfPurchase}</span>
+                <AddIcon
+                  onClick={() => setNumOfPurchase((prevNum) => (prevNum += 1))}
+                  style={{
+                    padding: '5px',
+                    backgroundColor: 'black',
+                    color: 'white',
+                    cursor: 'pointer',
+                  }}
+                ></AddIcon>
+              </div>
+            </div>
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginTop: '20px',
+              }}
+            >
+              <Button
+                variant="outlined"
+                style={{
+                  color: 'black',
+                  marginRight: '10px',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <ShoppingBasketIcon style={{ marginRight: '5px', width: '20px', height: '20px' }} />
+                <p>Add To Cart</p>
+              </Button>
+              <Button variant="contained" style={{ backgroundColor: 'black', color: 'white' }}>
+                Checkout
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{ display: 'flex', marginTop: '20px' }}>
+        <h3
+          onClick={() => setActiveTab(0)}
+          className={activeTab === 0 && classes.activeTab}
+          style={{
+            cursor: 'pointer',
+            fontSize: ' 16px',
+            margin: '10px 20px 0 0px',
+            color: 'gray',
+          }}
+        >
+          Description
+        </h3>
+        <h3
+          onClick={() => setActiveTab(1)}
+          className={activeTab === 1 && classes.activeTab}
+          style={{ cursor: 'pointer', fontSize: ' 16px', margin: '10px 0', color: 'gray' }}
+        >
+          Reviews
+        </h3>
+      </div>
+      <div>
+        {activeTab === 0 ? (
+          <p style={{ fontSize: '14px' }}>{product.description}</p>
+        ) : (
+          <p style={{ fontSize: '14px' }}>{product.reviews}</p>
+        )}
       </div>
     </div>
   );
