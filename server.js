69b029f201aa2c2314fbf54235c5eb1408d03a44
hiskio-daniel <daniel.yeh@hiskio.com>
2021-01-24 09:54:19 +0800

    transferred to new file structure

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const bodyParser = require('body-parser');
-const authRoute = require('./src/server/routes/authRoute');
-const userRoute = require('./src/server/routes/userRoute');
-const oAuthRoute = require('./src/server/routes/oAuthRoute');
-const productRoute = require('./src/server/routes/productRoute');
-const connectDB = require('./src/server/tools/db');
+const authRoute = require('./server/routes/authRoute');
+const userRoute = require('./server/routes/userRoute');
+const oAuthRoute = require('./server/routes/oAuthRoute');
+const productRoute = require('./server/routes/productRoute');
+const connectDB = require('./server/tools/db');
 
 // Basic requirements and setup
 require('dotenv').config();
