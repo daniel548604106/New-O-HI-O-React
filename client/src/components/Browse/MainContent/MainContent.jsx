4fb060e345e7b41512982de8100ebc79c8b18b2c
hiskio-daniel <daniel.yeh@hiskio.com>
2021-01-28 16:05:07 +0800

    browse page

--- client/src/components/Browse/MainContent/MainContent.jsx
+++ client/src/components/Browse/MainContent/MainContent.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import classes from './MainContent.module.scss';
-const MainContent = () => {
+import { useLocation } from 'react-router-dom';
+const MainContent = ({ activeCategory, categoryId, subcategoryId }) => {
+  const location = useLocation();
+  const [activeSubcategory, setActiveSubcategory] = useState([]);
+  useEffect(() => {
+    let subcategory;
+    if (activeCategory && subcategoryId) {
+      subcategory = activeCategory.category.find((option) => {
+        return option.id === subcategoryId;
+      });
+    }
+    console.log(subcategory);
+  }, [activeCategory, subcategoryId]);
   return (
     <>
-      <div>MainCOntent</div>
+      <div>{activeCategory ? <h2>{activeCategory.title}</h2> : <h2>今日熱門商品</h2>}</div>
     </>
   );
+};
+
+MainContent.propTypes = {
+  categoryId: PropTypes.string,
+  activeCategory: PropTypes.arrays,
+  subcategoryId: PropTypes.number,
 };
 
 export default MainContent;
