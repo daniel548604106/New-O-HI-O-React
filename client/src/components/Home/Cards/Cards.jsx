a034ee91b5a4dc593136fb627d30c619bd0306af
hiskio-daniel <daniel.yeh@hiskio.com>
2021-04-11 03:00:22 +0800

    changed home page show more btn link

--- client/src/components/Home/Cards/Cards.jsx
+++ client/src/components/Home/Cards/Cards.jsx
@@ -4,12 +4,17 @@ import ProductCard from '../../Global/ProductCard/ProductCard.jsx';
 import PropTypes from 'react';
 import Skeleton from 'react-loading-skeleton';
 import ProductCardLoading from '../../Global/SkeletonLoading/ProductCardLoading.jsx';
-const Cards = ({ products, title, t }) => {
+import { Link } from 'react-router-dom';
+const Cards = ({ products, title, t, link, showMore }) => {
   return (
     <div className={classes.cardSection}>
       <div className={classes.titleRow}>
         <h2 className={classes.title}>{t(title)}</h2>
-        <span className={classes.seeMore}>{t('seeMore')}</span>
+        {showMore && (
+          <Link to={link} className={classes.seeMore}>
+            {t('seeMore')}
+          </Link>
+        )}
       </div>
       {products.length ? (
         <div className={classes.cardRow}>
@@ -30,6 +35,12 @@ Cards.propTypes = {
   products: PropTypes.Array,
   t: PropTypes.func,
   title: PropTypes.string,
+  link: PropTypes.string,
+  showMore: PropTypes.bool,
+};
+
+Cards.defaultProps = {
+  showMore: true,
 };
 
 export default Cards;
